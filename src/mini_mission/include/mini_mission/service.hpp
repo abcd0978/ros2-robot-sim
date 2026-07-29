fda1ed91a1ec5c5a06d16178a5b526773ba76483
mgkim <mgkim@alux-platform.com>
2026-07-29 06:27:49 +0000

    등호 정렬

---
+++ src/mini_mission/include/mini_mission/service.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace mini_mission::service {
+    inline constexpr const char * RESET_POSE    = "reset_pose";
+    inline constexpr const char * START_MISSION = "start_mission";
+}
--- src/mini_mission/include/mini_mission/topic.hpp
+++ src/mini_mission/include/mini_mission/topic.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace mini_mission::topic {
-    inline constexpr const char * ODOM = "odom";
+    inline constexpr const char * ODOM         = "odom";
     inline constexpr const char * ROBOT_STATUS = "robot_status";
 }
