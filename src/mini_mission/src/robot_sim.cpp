004e845577929a815e5bc326453064532d2c55e5
mgkim <mgkim@alux-platform.com>
2026-07-29 08:51:12 +0000

    wip: step6 코어 코드 작성중

--- src/mini_mission/src/robot_sim.cpp
+++ src/mini_mission/src/robot_sim.cpp
@@ -14,13 +14,21 @@
 #include "mini_mission/param.hpp"
 #include "mini_mission/service.hpp"
 
+#include <cmath>
+#include <thread>
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "mini_mission_interfaces/action/navigate_to.hpp"
+
+using NavigateTo = mini_mission_interfaces::action::NavigateTo;
+using GoalHandle = rclcpp_action::ServerGoalHandle<NavigateTo>;
+
 class RobotSim : public rclcpp::Node
 {
 public:
   RobotSim()
   : Node("robot_sim")
   {
-    // publisher 생성
+    // 퍼블리셔 생성
     odom_pub_ = create_publisher<geometry_msgs::msg::Pose2D>(
       mini_mission::topic::ODOM, 
       rclcpp::QoS(rclcpp::SensorDataQoS())
@@ -31,16 +39,14 @@ public:
     );
     set_status(mini_mission::robot_status::IDLE);
 
-    // 파라미터 설정, 콜백 연결
+    // 파라미터 선언, 콜백 연결
     declare_parameter<double>(mini_mission::param::MAX_SPEED, 1.0);
     declare_parameter<double>(mini_mission::param::POS_TOLERANCE, 0.05);
     declare_parameter<double>(mini_mission::param::PUBLISH_RATE, 10.0);
     max_speed_     = get_parameter(mini_mission::param::MAX_SPEED).as_double();
     pos_tolerance_ = get_parameter(mini_mission::param::POS_TOLERANCE).as_double();
     publish_rate_  = get_parameter(mini_mission::param::PUBLISH_RATE).as_double();
-
     reset_timer();
-
     param_cb_ = add_on_set_parameters_callback(
             std::bind(&RobotSim::on_param_change, this, std::placeholders::_1));
 
@@ -48,6 +54,15 @@ public:
     reset_srv_ = create_service<std_srvs::srv::Trigger>(
       mini_mission::service::RESET_POSE,
       std::bind(&RobotSim::on_reset, this, std::placeholders::_1, std::placeholders::_2)
+    );
+
+    //액션서버 생성
+    nav_server_ = rclcpp_action::create_server<NavigateTo>(
+      this, "navigate_to",
+      std::bind(&RobotSim::handle_goal, this,
+        std::placeholders::_1, std::placeholders::_2),
+      std::bind(&RobotSim::handle_cancel, this, std::placeholders::_1),
+      std::bind(&RobotSim::handle_accepted, this, std::placeholders::_1)
     );
 
     // 로봇 노드 시작
@@ -157,22 +172,65 @@ private:
     odom_pub_->publish(msg);
   }
 
+  rclcpp_action::GoalResponse handle_goal(
+    const rclcpp_action::GoalUUID & uuid,
+    std::shared_ptr<const NavigateTo::Goal> goal)
+  {
+    // TODO: goal 좌표 검증. 거부하려면 GoalResponse::REJECT
+    (void)uuid; (void)goal;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse handle_cancel(
+    const std::shared_ptr<GoalHandle> goal_handle)
+  {
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+  }
+
+  void handle_accepted(const std::shared_ptr<GoalHandle> goal_handle)
+  {
+    // TODO: std::thread{std::bind(&RobotSim::execute, this, goal_handle)}.detach();
+    //       여기서 execute 를 직접 부르면 노드 전체가 멈춘다
+    (void)goal_handle;
+  }
+
+  void execute(const std::shared_ptr<GoalHandle> goal_handle)
+  {
+    // TODO: 루프
+    //   1) 목표까지 남은 거리 계산
+    //   2) pos_tolerance_ 이내면 결과 채우고 goal_handle->succeed(result) 후 종료
+    //   3) 아니면 max_speed_ * dt 만큼 전진, set_status("MOVING")
+    //   4) goal_handle->publish_feedback(feedback)
+    //   5) publish_rate_ 주기로 sleep 후 반복
+    (void)goal_handle;
+  }
+
+
   // 로봇의 위치 상태. 
   double x_{0.0}, y_{0.0}, theta_{0.0};
+
+  // 로봇의 상태
+  std::string status_{mini_mission::robot_status::NONE};
+
   //파라미터
   double max_speed_{1.0};
   double pos_tolerance_{0.05};
   double publish_rate_{10.0};
   rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr param_cb_;
 
+  //퍼블리셔
   rclcpp::Publisher<geometry_msgs::msg::Pose2D>::SharedPtr odom_pub_;
-  
-  rclcpp::TimerBase::SharedPtr timer_;
-
-  std::string status_{mini_mission::robot_status::NONE};
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr status_pub_;
 
+  //타이머
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  //서비스
   rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr reset_srv_;
+
+  // 액션 서버
+  rclcpp_action::Server<NavigateTo>::SharedPtr nav_server_;
 };
 
 int main(int argc, char ** argv)
